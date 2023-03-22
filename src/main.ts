f8be172fbaa8297aed58dfac636a46ffd1b14d83
Xif <Tmxif732@gmail.com>
2023-03-22 11:46:37 +0100

    0.0.6

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,30 @@ import { App, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 interface DailyTodoPluginSettings {
   templateFilePath: string;
+  enableNotifications: boolean;
+  notificationInterval: number; // en minutes
 }
 
 const DEFAULT_SETTINGS: DailyTodoPluginSettings = {
   templateFilePath: "",
+  enableNotifications: true,
+  notificationInterval: 60, // par défaut 60 minutes
 };
 
 export default class DailyTodoPlugin extends Plugin {
   settings: DailyTodoPluginSettings;
+  notificationTimer: number;
 
   async onload() {
     await this.loadSettings();
     this.app.workspace.on('layout-change', () => this.createDailyTodoNote());
     this.addSettingTab(new DailyTodoSettingTab(this.app, this));
+
+    this.startNotificationTimer();
+  }
+
+  async onunload() {
+    this.stopNotificationTimer();
   }
 
   async loadSettings() {
@@ -28,10 +39,10 @@ export default class DailyTodoPlugin extends Plugin {
   async readTemplateFile(): Promise<string> {
     const { vault } = this.app;
     const templateFile = vault.getAbstractFileByPath(this.settings.templateFilePath);
-  
+
     console.log("Template file path:", this.settings.templateFilePath);
     console.log("Template file:", templateFile);
-  
+
     if (templateFile instanceof TFile) {
       try {
         const content = await vault.read(templateFile);
@@ -43,30 +54,61 @@ export default class DailyTodoPlugin extends Plugin {
     }
     return `No template`;
   }
-  
 
   async createDailyTodoNote() {
-  const dateFormat = 'YYYY-MM-DD';
-  const folderName = 'Daily Todos';
-  const vault = this.app.vault;
+    const dateFormat = 'YYYY-MM-DD';
+    const folderName = 'Daily Todos';
+    const vault = this.app.vault;
 
-  const today = window.moment().format(dateFormat);
-  const dailyNoteName = `Todo - ${today}`;
+    const today = window.moment().format(dateFormat);
+    const dailyNoteName = `Todo - ${today}`;
 
-  const folder = vault.getAbstractFileByPath(folderName);
+    const folder = vault.getAbstractFileByPath(folderName);
 
-  if (!folder) {
-    await vault.createFolder(folderName);
+    if (!folder) {
+      await vault.createFolder(folderName);
+    }
+
+    const dailyNote = vault.getAbstractFileByPath(`${folderName}/${dailyNoteName}.md`);
+
+    if (!dailyNote) {
+      const dailyTodoContent = await this.readTemplateFile();
+      await vault.create(`${folderName}/${dailyNoteName}.md`, dailyTodoContent);
+    }
   }
 
-  const dailyNote = vault.getAbstractFileByPath(`${folderName}/${dailyNoteName}.md`);
+  async checkAndNotify() {
+    if (!this.settings.enableNotifications) {
+      return;
+    }
 
-  if (!dailyNote) {
-    const dailyTodoContent = await this.readTemplateFile();
-    await vault.create(`${folderName}/${dailyNoteName}.md`, dailyTodoContent);
+    const dateFormat = 'YYYY-MM-DD';
+    const folderName = 'Daily Todos';
+    const vault = this.app.vault;
+
+    const today = window.moment().format(dateFormat);
+    const dailyNoteName = `Todo - ${today}`;
+
+    const dailyNote = vault.getAbstractFileByPath(`${folderName}/${dailyNoteName}.md`);
+
+    if (!dailyNote) {
+      new Notification("Daily Todo", {
+        body: "N'oubliez pas de créer votre liste de tâches quotidiennes !",
+      });
+    }
   }
-}
-  
+
+  startNotificationTimer() {
+    const interval = this.settings.notificationInterval * 60 * 1000;
+    this.notificationTimer = window.setInterval(() => this.checkAndNotify(), interval);
+  }
+
+  stopNotificationTimer() {
+    if (this.notificationTimer) {
+      window.clearInterval(this.notificationTimer);
+      this.notificationTimer = null;
+    }
+  }
 }
 
 class DailyTodoSettingTab extends PluginSettingTab {
@@ -91,6 +133,29 @@ class DailyTodoSettingTab extends PluginSettingTab {
         .onChange(async (value) => {
           this.plugin.settings.templateFilePath = value;
           await this.plugin.saveSettings();
+        }));
+
+    new Setting(containerEl)
+      .setName('Enable notifications')
+      .setDesc('Enable or disable daily todo notifications')
+      .addToggle(toggle => toggle
+        .setValue(this.plugin.settings.enableNotifications)
+        .onChange(async (value) => {
+          this.plugin.settings.enableNotifications = value;
+          await this.plugin.saveSettings();
+        }));
+
+    new Setting(containerEl)
+      .setName('Notification interval')
+      .setDesc('Interval between notifications in minutes')
+      .addSlider(slider => slider
+        .setLimits(1, 240, 1)
+        .setValue(this.plugin.settings.notificationInterval)
+        .onChange(async (value) => {
+          this.plugin.settings.notificationInterval = value;
+          await this.plugin.saveSettings();
+          this.plugin.stopNotificationTimer();
+          this.plugin.startNotificationTimer();
         }));
   }
 }
